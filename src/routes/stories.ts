c8323a6692c468aaa3e8c1f3545a37aa0352c443
techstyleanalytics <techstyleanalytics@gmail.com>
2023-02-26 14:17:45 -0600

    removed top dates

--- src/routes/stories.ts
+++ src/routes/stories.ts
@@ -17,11 +17,7 @@ routes.get("/", async (req, res) => {
 
 
 
-const timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
-var currentDate = new Date().toLocaleString('en-US', { timeZone: timezone }).split(',')[0].split('/');
-currentDate = currentDate[2] + '-' + currentDate[0].padStart(2, '0') + '-' + currentDate[1].padStart(2, '0');
-
-  const dateToFind = currentDate.join(''); // the date string to find in storyData
+  const dateToFind = '2023-02-26'; // the date string to find in storyData
   const foundStory = storyData.find((story) => story.date === dateToFind);
   const emptyBodyTitle = "This story is currently being written";
   const emptyBodyContent = "Come back in a few, it might be fortold soon";
