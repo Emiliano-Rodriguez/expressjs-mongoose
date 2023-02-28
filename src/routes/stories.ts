722351d559447ca9aff7c33618c7b5774bff1125
techstyleanalytics <techstyleanalytics@gmail.com>
2023-02-27 18:38:26 -0600

    trying function

--- src/routes/stories.ts
+++ src/routes/stories.ts
@@ -16,8 +16,24 @@ routes.get("/", async (req, res) => {
   });
 
 
+const timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+var currentDate = new Date().toLocaleString('en-US', { timeZone: timezone }).split(',')[0].split('/');
+currentDate = currentDate[2] + '-' + currentDate[0].padStart(2, '0') + '-' + currentDate[1].padStart(2, '0');
 
+dateToFind = currentDate;
+console.log(dateToFind);
+  function getTitleAndContentByDate(dateToFind, storyData) {
+    const emptyBodyTitle = "This story is currently being written";
+    const emptyBodyContent = "Come back in a few, it might be fortold soon";
   
+    const foundStory = storyData.find((story) => story.date === dateToFind);
+    const title = foundStory ? foundStory.title : emptyBodyTitle;
+    const content = foundStory ? foundStory.content : emptyBodyContent;
+    return { title, content };
+  }
+const { title, content } = getTitleAndContentByDate(dateToFind, storyData);
+
+/**
   const dateToFind = '2023-02-26'; // the date string to find in storyData
   const foundStory = storyData.find((story) => story.date === dateToFind);
   const emptyBodyTitle = "This story is currently being written";
@@ -28,7 +44,7 @@ routes.get("/", async (req, res) => {
   const title = foundStory ? foundStory.title : emptyBodyTitle;
   const content = foundStory ? foundStory.content : emptyBodyContent;
   
-
+*/
     // Build an HTML string with the title and stories
     const html = `
 
